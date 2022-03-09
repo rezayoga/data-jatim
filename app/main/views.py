1fe0186d4b7f3bf11da8366ce7a7c4de56ac8c9a
Reza Yogaswara <reza.yoga@gmail.com>
2022-03-09 23:28:47 +0700

    Updating option path params views.py

--- app/main/views.py
+++ app/main/views.py
@@ -11,8 +11,8 @@ os.environ['TZ'] = 'Asia/Jakarta'
 time.tzset()
 
 
-@main_blueprint.route('/ptsl/', defaults={'year': None, 'month': None, 'date': None})
-@main_blueprint.route('/ptsl/<string:year>/<string:month>/',  defaults={'date': None})
+@main_blueprint.route('/ptsl', defaults={'year': None, 'month': None, 'date': None})
+@main_blueprint.route('/ptsl/<string:year>/<string:month>',  defaults={'date': None})
 @main_blueprint.route('/ptsl/<string:year>/<string:month>/<string:date>')
 def ptsl(year=None, month=None, date=None):
     data = Ptsl.query.filter_by(y=time.strftime(
@@ -20,7 +20,7 @@ def ptsl(year=None, month=None, date=None):
     if year != None and month != None and date != None:
         data = Ptsl.query.filter_by(y=year, m=month, d=date).all()
     elif year != None and month != None and date == None:
-        abort(500)
+        abort(404)
     #current_app.logger.info("Index page loading")
     return render_template('index.html', data=data)
 
