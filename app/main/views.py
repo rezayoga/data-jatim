2d1137b7b47ee47b622625a534dc387fde67a47a
Reza Yogaswara <reza.yoga@gmail.com>
2022-03-09 23:25:10 +0700

    Updating views.py

--- app/main/views.py
+++ app/main/views.py
@@ -6,14 +6,17 @@ import datetime
 import calendar
 import os
 now = datetime.datetime.now()
-#print now.year, now.month, now.day, now.hour, now.minute, now.second
+# print now.year, now.month, now.day, now.hour, now.minute, now.second
 os.environ['TZ'] = 'Asia/Jakarta'
 time.tzset()
 
+
 @main_blueprint.route('/ptsl/', defaults={'year': None, 'month': None, 'date': None})
+@main_blueprint.route('/ptsl/<string:year>/<string:month>/',  defaults={'date': None})
 @main_blueprint.route('/ptsl/<string:year>/<string:month>/<string:date>')
 def ptsl(year=None, month=None, date=None):
-    data = Ptsl.query.filter_by(y=time.strftime('%Y'), m=time.strftime('%m'), d=time.strftime('%d')).all()
+    data = Ptsl.query.filter_by(y=time.strftime(
+        '%Y'), m=time.strftime('%m'), d=time.strftime('%d')).all()
     if year != None and month != None and date != None:
         data = Ptsl.query.filter_by(y=year, m=month, d=date).all()
     elif year != None and month != None and date == None:
