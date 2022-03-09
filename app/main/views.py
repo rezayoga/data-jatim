bdc02e5670bc03d62ca8e5fbd5be43a451c9c926
Reza Yogaswara <reza.yoga@gmail.com>
2022-03-10 00:03:00 +0700

    Updating views.py

--- app/main/views.py
+++ app/main/views.py
@@ -19,7 +19,7 @@ def ptsl(year=None, month=None, date=None):
     if year != None and month != None and date != None:
         data = Ptsl.query.filter_by(y=year, m=month, d=date).all()
     elif year != None and month != None and date == None:
-        pass
+        abort(404)
     #current_app.logger.info("Index page loading")
     return render_template('index.html', data=data)
 
