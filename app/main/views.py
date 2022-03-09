976af8b15d39a6e700fac027bfcb38a33b4b10d6
Reza Yogaswara <reza.yoga@gmail.com>
2022-03-09 23:09:16 +0700

    Updating views.py

--- app/main/views.py
+++ app/main/views.py
@@ -14,8 +14,10 @@ time.tzset()
 @main_blueprint.route('/ptsl/<string:year>/<string:month>/<string:date>')
 def ptsl(year=None, month=None, date=None):
     data = Ptsl.query.filter_by(y=time.strftime('%Y'), m=time.strftime('%m'), d=time.strftime('%d')).all()
-    if year != None and month != None:
+    if year != None and month != None and date != None:
         data = Ptsl.query.filter_by(y=year, m=month, d=date).all()
+    elif year != None and month != None and date == None:
+        abort(500)
     #current_app.logger.info("Index page loading")
     return render_template('index.html', data=data)
 
