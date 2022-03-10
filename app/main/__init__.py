f8269710f2afef357fe68d986588de1737b3bc31
Reza Yogaswara <me@rezayogaswara.com>
2022-03-10 11:39:19 +0700

    Updating

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,6 +1,6 @@
 #This bluepint will deal with all user management functionality
 from flask import Blueprint
-main_blueprint = Blueprint('main', __name__, template_folder='templates', static_folder='static')
+main_blueprint = Blueprint('main', __name__, template_folder='templates', static_folder='static', static_url_path='/static')
 
 from . import views
 
