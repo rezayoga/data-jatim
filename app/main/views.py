55314e91eeccd6cdb0af3d5a3a492da8f2573890
Reza Yogaswara <me@rezayogaswara.com>
2022-03-10 10:21:09 +0700

    Updating

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,12 @@
-from . import main_blueprint
-from flask import render_template, request, redirect, url_for, current_app, abort
-from app.models import Ptsl
-import time
 import datetime
-import calendar
 import os
+import time
+
+from flask import render_template, abort
+
+from app.models import Ptsl
+from . import main_blueprint
+
 now = datetime.datetime.now()
 # print now.year, now.month, now.day, now.hour, now.minute, now.second
 os.environ['TZ'] = 'Asia/Jakarta'
@@ -12,7 +14,7 @@ time.tzset()
 
 
 @main_blueprint.route('/ptsl', defaults={'year': None, 'month': None, 'date': None})
-@main_blueprint.route('/ptsl/<year>/<month>',  defaults={'date': None})
+@main_blueprint.route('/ptsl/<year>/<month>', defaults={'date': None})
 @main_blueprint.route('/ptsl/<year>/<month>/<date>')
 def ptsl(year=None, month=None, date=None):
     data = Ptsl.query.filter_by(y=time.strftime('%Y'), m=time.strftime('%m'), d=time.strftime('%d')).all()
@@ -20,7 +22,7 @@ def ptsl(year=None, month=None, date=None):
         data = Ptsl.query.filter_by(y=year, m=month, d=date).all()
     elif year != None and month != None and date == None:
         abort(404)
-    #current_app.logger.info("Index page loading")
+    # current_app.logger.info("Index page loading")
     return render_template('index.html', data=data)
 
 
