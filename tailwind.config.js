d52677cd994511d890f6b2a5135405a8a32c0d9f
Reza Yogaswara <reza.yoga@gmail.com>
2022-03-12 20:44:13 +0700

    Updating

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,45 @@
 // tailwind.config.js
 module.exports = {
-  content: [
-    "./app/main/templates/*.{html,js}",
-  ],
-  darkMode: 'media', // or 'media' or 'class'
+  content: ["./app/main/templates/*.{html,js}"],
+  darkMode: "media", // or 'media' or 'class'
   //theme: {},
   variants: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-    require("daisyui")
-  ],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
     styled: true,
-    themes: ["dark", "emerald", "light", "cupcake", "bumblebee", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+    themes: [
+      "emerald",
+      "dark",
+      "light",
+      "cupcake",
+      "bumblebee",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "garden",
+      "forest",
+      "aqua",
+      "lofi",
+      "pastel",
+      "fantasy",
+      "wireframe",
+      "black",
+      "luxury",
+      "dracula",
+      "cmyk",
+      "autumn",
+      "business",
+      "acid",
+      "lemonade",
+      "night",
+      "coffee",
+      "winter",
+    ],
     base: true,
     utils: true,
     logs: true,
