639e510e20841a77b1d03ac2623c37ae4fccd151
Reza Yogaswara <reza.yoga@gmail.com>
2022-03-31 08:05:43 +0700

    Updating SHAT kemarin

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,7 +1,17 @@
 # This bluepint will deal with all user management functionality
 from flask import Blueprint
+from sqlalchemy import text, create_engine
+from app.models import Ptsl, KualitasDataLengkap, DataSiapElektronik, RekapWarkahDigital
 
 main_blueprint = Blueprint('main', __name__, template_folder='templates', static_folder='static',
                            static_url_path='/main.static')
 
+def get_data(string_sql):
+    engine = create_engine(
+        "mysql+pymysql://root:rezareza1985@localhost/db_data_jatim")
+    sql = text(string_sql)
+    result = engine.execute(sql).fetchone()
+    return result
+
+main_blueprint.add_app_template_global(get_data, 'get_data')
 from . import views
--- app/main/views.py
+++ app/main/views.py
@@ -2,10 +2,11 @@ from . import main_blueprint
 from app.models import Ptsl, KualitasDataLengkap, DataSiapElektronik, RekapWarkahDigital
 import io
 import random
-from flask import render_template, abort, redirect, url_for, Response, request
+from flask import render_template, abort, redirect, url_for, Response, request, session
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 import datetime
+from datetime import timedelta
 import os
 import time
 import pymysql
@@ -30,9 +31,19 @@ def index():
 @main_blueprint.route('/ptsl/<year>/<month>', defaults={'date': None})
 @main_blueprint.route('/ptsl/<year>/<month>/<date>')
 def ptsl(year=None, month=None, date=None):
+
     y = time.strftime('%Y')
     m = time.strftime('%m')
     d = time.strftime('%d')
+    
+    #session.pop('y', None)
+    #session.pop('m', None)
+    #session.pop('d', None)
+    
+
+    y_now = time.strftime('%Y')
+    m_now = time.strftime('%m')
+    d_now = time.strftime('%d')
 
     data = Ptsl.query.filter_by(y=y, m=m, d=d).all()
     if year != None and month != None and date != None:
@@ -42,11 +53,44 @@ def ptsl(year=None, month=None, date=None):
         data = Ptsl.query.filter_by(y=year, m=month, d=date).all()
     elif year != None and month != None and date == None:
         abort(404)
+
+    if session.get('y') == None:
+        session['y'] = y
+        session['m'] = '09'
+        session['d'] = '01'
+
+    finish_date = datetime.datetime(int(session['y']), int(session['m']), int(session['d']))
+    start_date = datetime.datetime(int(y_now), int(m_now), int(d_now))
+    
+    progress_date_yesterday = datetime.datetime(int(y), int(m), int(d)) - timedelta(days=1)
+    
+    #print(f"{progress_date_yesterday.strftime('%Y')}-{progress_date_yesterday.strftime('%m')}-{progress_date_yesterday.strftime('%d')}")
+    
+    
+    if finish_date < start_date:
+        abort(404)
+
+    delta = finish_date - start_date
+    days = delta.days
+    # print(f"{session['y']}-{session['m']}-{session['d']}")
+
     # current_app.logger.info("Index page loading")
     shat = request.args.get('shat') if request.args.get(
         'shat') != None else 'puldadis'
-    return render_template('index.html', data=data, y=y, m=m, d=d, base_url='/ptsl', shat=shat)
+    return render_template('index.html', data=data, y=y, m=m, d=d, yy=progress_date_yesterday.strftime('%Y'), my=progress_date_yesterday.strftime('%m'), dy=progress_date_yesterday.strftime('%d'), days=days, progress_date_yesterday=progress_date_yesterday, base_url='/ptsl', shat=shat)
 
+
+@main_blueprint.route('/ptsl/set_finish_date', methods=['POST'])
+def set_finish_date():
+    
+    date = request.form['finish_date'].split("/")
+    session['y'] = date[0]
+    session['m'] = date[1]
+    session['d'] = date[2]
+    
+    #return f"{session['y']}-{session['m']}-{session['d']}"
+    
+    return redirect(url_for('main.ptsl'))
 
 @main_blueprint.route('/ptsl/graph/<shat>/<type>')
 def trend(shat, type):
