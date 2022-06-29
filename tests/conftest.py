59e8ef6578bbef89d1a0ebdcf15dcef65154953d
Reza Yogaswara <reza.yoga@gmail.com>
2022-06-29 10:43:58 +0700

    Updating

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,26 +1,40 @@
 import pytest
 
 from app import create_app
-from app.models import db
+from app.models import db, Ptsl
+
+@pytest.fixture(scope='module')
+def new_ptsl():
+    ptsl = Ptsl('Kabupaten Malang', 1, 2, 3)
+    return ptsl
 
 
 @pytest.fixture(scope='module')
 def test_client():
-    app = create_app()
-    app.config.from_object('config.DevelopmentConfig')
-    with app.test_client() as testing_client:
+    flask_app = create_app()
+    flask_app.config.from_object('config.TestingConfig')
+
+    # Create a test client using the Flask application configured for testing
+    with flask_app.test_client() as testing_client:
         # Establish an application context
-        with app.app_context():
-            yield testing_client
+        with flask_app.app_context():
+            yield testing_client  # this is where the testing happens!
 
 
-"""
 @pytest.fixture(scope='module')
-def test_database():
-    app = create_app()
-    db.init_app(app)
+def init_database(test_client):
+    # Create the database and the database table
     db.create_all()
-    yield db  # testing happens here
-    db.session.remove()
+
+    # Insert user data
+    ptsl1 = Ptsl('Kabupaten Malang', 1, 2, 3)
+    ptsl2 = Ptsl('Kota Batu', 4, 5, 6)
+    db.session.add(ptsl1)
+    db.session.add(ptsl2)
+
+    # Commit the changes for the users
+    db.session.commit()
+
+    yield  # this is where the testing happens!
+
     db.drop_all()
-"""
--- tests/functional/test_ptsl.py
+++ tests/functional/test_ptsl.py
@@ -1,3 +1,36 @@
+from app import create_app
+
+
+def test_home_page():
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/' page is requested (GET)
+    THEN check that the response is valid
+    """
+    flask_app = create_app()
+    flask_app.config.from_object('config.DevelopmentConfig')
+
+    # Create a test client using the Flask application configured for testing
+    with flask_app.test_client() as test_client:
+        response = test_client.get('/')
+        assert response.status_code == 200
+
+
+def test_home_page_post():
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/' page is is posted to (POST)
+    THEN check that a '405' status code is returned
+    """
+    flask_app = create_app()
+    flask_app.config.from_object('config.DevelopmentConfig')
+
+    # Create a test client using the Flask application configured for testing
+    with flask_app.test_client() as test_client:
+        response = test_client.post('/')
+        assert response.status_code == 405
+
+
 def test_home_page_with_fixture(test_client):
     """
     GIVEN a Flask application configured for testing
@@ -10,7 +43,7 @@ def test_home_page_with_fixture(test_client):
 
 def test_home_page_post_with_fixture(test_client):
     """
-    GIVEN a Flask application
+    GIVEN a Flask application configured for testing
     WHEN the '/' page is is posted to (POST)
     THEN check that a '405' status code is returned
     """
