e6fe7a1454f4596ec7d6eadd29e745b8440b6ee7
Reza Yogaswara <reza.yoga@gmail.com>
2022-06-29 10:51:12 +0700

    Updating

--- tests/functional/test_ptsl.py
+++ tests/functional/test_ptsl.py
@@ -8,12 +8,13 @@ def test_home_page():
     THEN check that the response is valid
     """
     flask_app = create_app()
-    flask_app.config.from_object('config.DevelopmentConfig')
+    flask_app.config.from_object('config.TestingConfig')
 
     # Create a test client using the Flask application configured for testing
     with flask_app.test_client() as test_client:
         response = test_client.get('/')
         assert response.status_code == 200
+        assert b"Progres PTSL 2k22" in response.data
 
 
 def test_home_page_post():
@@ -23,7 +24,7 @@ def test_home_page_post():
     THEN check that a '405' status code is returned
     """
     flask_app = create_app()
-    flask_app.config.from_object('config.DevelopmentConfig')
+    flask_app.config.from_object('config.TestingConfig')
 
     # Create a test client using the Flask application configured for testing
     with flask_app.test_client() as test_client:
@@ -39,6 +40,7 @@ def test_home_page_with_fixture(test_client):
     """
     response = test_client.get('/')
     assert response.status_code == 200
+    assert b"Progres PTSL 2k22" in response.data
 
 
 def test_home_page_post_with_fixture(test_client):
